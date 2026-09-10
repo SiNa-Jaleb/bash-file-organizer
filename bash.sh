5e1a4e1ba1a0ec6ce99c7febc6130edba88c5ab9
SiNa <sina885jaleb@gmail.com>
2026-09-11 00:49:04 +0330

    feat: add file operation logging

--- bash.sh
+++ bash.sh
@@ -3,15 +3,16 @@
 path=$1
 
 if [ -d $path ];then
-    mkdir $path/Images 
-    mkdir $path/Documents 
-    mkdir $path/Videos 
-    mkdir $path/Archives
-    mkdir $path/Others
+    mkdir "$path/Images" 
+    mkdir "$path/Documents" 
+    mkdir "$path/Videos" 
+    mkdir "$path/Archives"
+    mkdir "$path/Others"
+
     
     images_number=0
     documents_number=0
-    vidoes_number=0
+    videos_number=0
     archives_number=0
     others_number=0
 
@@ -22,22 +23,27 @@ if [ -d $path ];then
             *.png|*.jpg|*.jpeg)
                 mv "$file" "$path/Images/"
                 images_number=$((1 + images_number))
+                echo "[ $(date) ] Moved $file ==> Images" >> "$path/log.txt"
                 ;;
             *.pdf|*.txt)
                 mv "$file" "$path/Documents"
                 documents_number=$((1 + documents_number))
+                echo "[ $(date) ] Moved $file ==> Documents" >> "$path/log.txt"
                 ;;
             *.mp4|*.mkv)
                 mv "$file" "$path/Videos"
                 videos_number=$((1 + videos_number))
+                echo "[ $(date ]) Moved $file ==> Videos" >> "$path/log.txt"
                 ;;
             *.zip|*.rar)
                 mv "$file" "$path/Archives"
                 archives_number=$((1 + archives_number))
+                echo "[ $(date) ] Moved $file ==> Archives" >> "$path/log.txt"
                 ;;
             *)
                  mv "$file" "$path/Others"
                 others_number=$((1 + others_number))
+                echo "[ $(date) ] Moved $file ==> Others" >> "$path/log.txt"
                 ;;
 
             esac
