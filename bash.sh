d8a66abb59a768b1d356548830c7e833542c18da
SiNa <sina885jaleb@gmail.com>
2026-09-11 02:25:33 +0330

    feat: add duplicate filename handling

--- bash.sh
+++ bash.sh
@@ -2,6 +2,31 @@
 
 path=$1
 
+get_uniq_name() {
+    file=$1
+
+    if [! -e "$file"];then
+        echo "$file"
+        return
+    fi
+
+    direc=$(dirname "$file")
+    filename=$(basename "$file")
+    
+    name=${filename%.*}
+    pasvand=${filename##*.}
+
+    counter=1
+
+    while [ -e "$direc/${name}_$counter.$pasvand" ]
+    do
+        counter=$((1 + counter))
+    done
+    
+    echo "$direc/${name}_$counter.$pasvand"
+}
+
+
 if [ -d $path ];then
     mkdir "$path/Images" 
     mkdir "$path/Documents" 
@@ -21,27 +46,37 @@ if [ -d $path ];then
         if [ -f "$file" ];then
             case "$file" in 
             *.png|*.jpg|*.jpeg)
-                mv "$file" "$path/Images/"
+                dest="$path/Images/$(basename "$file")"
+                dest=$(get_uniq_name "$dest")
+                mv "$file" "$dest"
                 images_number=$((1 + images_number))
                 echo "[ $(date) ] Moved $file ==> Images" >> "$path/log.txt"
                 ;;
             *.pdf|*.txt)
-                mv "$file" "$path/Documents"
+                dest="$path/Documents/$(basename "$file")"
+                dest=$(get_uniq_name "$dest")
+                mv "$file" "$dest"
                 documents_number=$((1 + documents_number))
                 echo "[ $(date) ] Moved $file ==> Documents" >> "$path/log.txt"
                 ;;
             *.mp4|*.mkv)
-                mv "$file" "$path/Videos"
+                dest="$path/Videos/$(basename "$file")"
+                dest=$(get_uniq_name "$dest")
+                mv "$file" "$dest"
                 videos_number=$((1 + videos_number))
                 echo "[ $(date ]) Moved $file ==> Videos" >> "$path/log.txt"
                 ;;
             *.zip|*.rar)
-                mv "$file" "$path/Archives"
+                dest="$path/Archives/$(basename "$file")"
+                dest=$(get_uniq_name "$dest")
+                mv "$file" "$dest"
                 archives_number=$((1 + archives_number))
                 echo "[ $(date) ] Moved $file ==> Archives" >> "$path/log.txt"
                 ;;
             *)
-                 mv "$file" "$path/Others"
+                dest="$path/Others/$(basename "$file")"
+                dest=$(get_uniq_name "$dest")
+                 mv "$file" "$dest"
                 others_number=$((1 + others_number))
                 echo "[ $(date) ] Moved $file ==> Others" >> "$path/log.txt"
                 ;;
